c321bd9611512421c731396185bea79d6e4b26eb
Ben <bendb923@gmail.com>
2016-02-05 12:37:47 +0000

    Updated Boss Explosions! :family:

--- SMFL/Boss.cpp
+++ SMFL/Boss.cpp
@@ -16,6 +16,15 @@ const sf::IntRect Boss::m_TOWER_TEX_RECT = sf::IntRect(98, 126, 28, 28);
 
 const sf::IntRect Boss::m_BOSS_SIZE = sf::IntRect(50, 50, 400, 100);
 
+const float Boss::m_DEATHANIMTIMER = 60000;
+const float Boss::m_MAXEXPFRAMES = 7;
+const int Boss::m_NUM_EXPLOSIONS = 5;
+
+
+const int Boss::m_EXPSIZE = 40;
+const int Boss::m_TEXOFFSETY = 156;
+
+
 
 
 Boss::Boss(sf::Texture * p_tex, sf::IntRect p_texRect)
@@ -28,12 +37,37 @@ Boss::Boss(sf::Texture * p_tex, sf::IntRect p_texRect)
 
 	m_velocity = sf::Vector2f(0, 0.0001);
 
+	m_animHasFinished = false;
+	m_state = BossState::ALIVE;
+	m_counterForAnim = 0;
+	m_currentFrame = 0;
+
 	m_towerList = std::vector<BossTower>();
 	m_weakSpots = std::vector<WeakSpot>();
+	m_explosionSprites = std::vector<Boss::ExplosionAnim>();
+
+	for (int i = 0; i < m_NUM_EXPLOSIONS; i++)
+	{
+		ExplosionAnim tempexpl;
+		sf::Sprite tempPushSpr = sf::Sprite(*p_tex);
+		sf::IntRect newRect;
+		newRect = sf::IntRect(m_EXPSIZE * m_currentFrame, m_TEXOFFSETY, m_EXPSIZE, m_EXPSIZE);
+		tempPushSpr.setTextureRect(newRect);
+		tempPushSpr.setScale(4, 4);
+		tempexpl.my_position = sf::Vector2f(m_END_POS.x + ((rand() % 400) - 300), m_END_POS.y + (rand() % 100)); (m_position);
+		tempexpl.my_sprite.setPosition(tempexpl.my_position);
+		tempexpl.my_sprite = tempPushSpr;
+
+		tempexpl.my_currentFrame = rand() % (int)(m_MAXEXPFRAMES);
+		tempexpl.my_counterForAnim = (rand() % (int(m_DEATHANIMTIMER)));
+
+		m_explosionSprites.push_back(tempexpl);
+	}
 
 	m_towerList.push_back(BossTower(m_TOWER_POS_1, p_tex, m_TOWER_TEX_RECT, m_TOWER_COLL_BOX_1));
 	m_towerList.push_back(BossTower(m_TOWER_POS_2, p_tex, m_TOWER_TEX_RECT, m_TOWER_COLL_BOX_2));
 	m_towerList.push_back(BossTower(m_TOWER_POS_3, p_tex, m_TOWER_TEX_RECT, m_TOWER_COLL_BOX_3));
+
 }
 
 Boss::Boss()
@@ -63,6 +97,24 @@ void Boss::Update(sf::Vector2f p_playerPos, float p_dt)
 	{
 		m_towerList.at(i).Update(p_playerPos, p_dt);
 	}
+
+	switch (m_state)
+	{
+	case BossState::ALIVE:
+		break;
+	case BossState::PLAYING_ANIM:
+
+		for (int i = 0; i < m_explosionSprites.size(); i++)
+		{
+			m_explosionSprites.at(i).UpdateAnim(p_dt);
+		}
+
+		break;
+	case BossState::DEAD:
+		break;
+
+	}
+
 }
 
 void Boss::Init()
@@ -74,16 +126,28 @@ std::vector<sf::Sprite> Boss::GetTextureList()
 {
 	std::vector<sf::Sprite> spriteList;
 
-	spriteList.push_back(m_sprite);
+	if (m_state == BossState::ALIVE)
+	{
+		spriteList.push_back(m_sprite);
 
-	for (int i = 0; i < m_towerList.size(); i++)
-	{
-		spriteList.push_back(*m_towerList.at(i).GetTexture());
+		for (int i = 0; i < m_towerList.size(); i++)
+		{
+			spriteList.push_back(*m_towerList.at(i).GetTexture());
+		}
+		for (int i = 0; i < m_weakSpots.size(); i++)
+		{
+			spriteList.push_back(*m_weakSpots.at(i).GetTexture());
+		}
 	}
-	for (int i = 0; i < m_weakSpots.size(); i++)
+	else
 	{
-		spriteList.push_back(*m_weakSpots.at(i).GetTexture());
+		spriteList.push_back(m_sprite);
+		for (int i = 0; i < m_NUM_EXPLOSIONS; i++)
+		{
+			spriteList.push_back(m_explosionSprites.at(i).my_sprite);
+		}
 	}
+	
 
 	return spriteList;
 }
@@ -103,6 +167,21 @@ bool Boss::CheckIfDefeated()
 			isDefeated = false;
 		}
 	}
+
+	if (m_state == BossState::ALIVE && isDefeated)
+	{
+		m_state = BossState::PLAYING_ANIM;
+		isDefeated = false;
+	}
+	else if (m_state == BossState::PLAYING_ANIM)
+	{
+		isDefeated = false;
+	}
+	else if (m_state == BossState::DEAD)
+	{
+		isDefeated = true;
+	}
+
 
 	return isDefeated;
 }
--- SMFL/Boss.h
+++ SMFL/Boss.h
@@ -4,6 +4,38 @@
 #include "WeakSpot.h"
 class Boss
 {
+
+	class ExplosionAnim
+	{
+	public:
+		int my_EXPSIZE = 40;
+		float my_counterForAnim;
+		float my_currentFrame;
+
+		sf::Sprite my_sprite;
+		sf::Vector2f my_position;
+
+		void UpdateAnim(float p_dt)
+		{
+			my_counterForAnim += p_dt;
+			if (my_counterForAnim > m_DEATHANIMTIMER)
+			{
+				my_currentFrame++;
+				my_counterForAnim = 0;
+				if (my_currentFrame > m_MAXEXPFRAMES)
+				{
+					//m_state = BossState::DEAD;
+					my_currentFrame = 0;
+					my_position = sf::Vector2f(m_END_POS.x + ((rand() % 400) - 300), m_END_POS.y + (rand() % 100));
+				}
+				sf::IntRect newRect = sf::IntRect(my_EXPSIZE * my_currentFrame, m_TEXOFFSETY, my_EXPSIZE, my_EXPSIZE);
+				my_sprite.setTextureRect(newRect);
+				my_sprite.setScale(4, 4);
+				my_sprite.setPosition(my_position);
+			}
+		}
+	};
+
 public:
 	Boss();
 	Boss(sf::Texture * p_tex, sf::IntRect p_texRect);
@@ -20,8 +52,30 @@ private:
 	std::vector<BossTower> m_towerList;
 	std::vector<WeakSpot> m_weakSpots;
 	sf::Sprite m_sprite;
+	std::vector<Boss::ExplosionAnim> m_explosionSprites;
 	sf::Vector2f m_position;
 	sf::Vector2f m_velocity;
+
+	//////////////////////////////////////////////////////////////////////////
+	//animation stuffs
+	enum BossState
+	{
+		ALIVE,
+		PLAYING_ANIM,
+		DEAD
+	};
+
+	BossState m_state;
+	bool m_animHasFinished;
+	float m_counterForAnim;
+	float m_currentFrame;
+
+	static const int m_NUM_EXPLOSIONS;
+	static const float m_DEATHANIMTIMER;
+	static const float m_MAXEXPFRAMES;
+	static const int m_EXPSIZE;
+	static const int m_TEXOFFSETY;
+	//////////////////////////////////////////////////////////////////////////
 
 	static const sf::Vector2f m_END_POS;
 	static const sf::Vector2f m_START_POS;
